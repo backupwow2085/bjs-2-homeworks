f1b87fa2485addee64369c9d181c50b522328f3b
backupwow2085 <backupwow2085@gmail.com>
2026-03-13 08:19:59 +0300

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,26 @@
 "use strict"
+
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  const discriminant = b ** 2 - 4 * a * c;
+
+  if (discriminant < 0) {
+    return [];
+  } else if (discriminant === 0) {
+    arr.push(-b / (2 * a));
+  } else {
+    const firstRoot = (-b + Math.sqrt(discriminant)) / (2 * a);
+    arr.push(firstRoot);
+    const secondRoot = (-b - Math.sqrt(discriminant)) / (2 * a);
+    arr.push(secondRoot);
+  }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  const monthlyRate = percent / 100 / 12;
+  const body = amount - contribution;
+  const monthlyPayment = body * (monthlyRate + (monthlyRate / ((1 + monthlyRate) ** countMonths - 1)));
+  const totalPayment = monthlyPayment * countMonths;
+  return parseFloat(totalPayment.toFixed(2))
 }
