98b51ec4d4d23af7c32b626e4e8eda78ea3080d4
backupwow2085 <backupwow2085@gmail.com>
2026-04-01 16:55:51 +0300

    Update task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,24 @@
 function compareArrays(arr1, arr2) {
-  
+  if (arr1.length === arr2.length) {
+    const valuesEqual = arr1.every((item, index) => item === arr2[index]);
+    return valuesEqual
+  } else {
+    return false;
+  }
 }
 
+
 function getUsersNamesInAgeRange(users, gender) {
-  
+  if (users.length === 0) {
+    return 0
+  } else {
+    let resultGender = users.filter((item) => item.gender === gender);
+    if (resultGender.length === 0) {
+      return 0
+    } else {
+      let resultAge = resultGender.map((one) => one.age);
+      let resultAgeSum = resultAge.reduce((acc, age) => acc + age);
+      return resultAgeSum / resultAge.length
+    }
+  }
 }
