41cc146a69970246a6ebefd510b92355e7121175
backupwow2085 <backupwow2085@gmail.com>
2026-04-10 12:35:35 +0300

    Update task.js

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,61 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
-}
+  let cache = [];
 
+  function wrapper(...args) {
+    const hash = md5(args);
+    const objectInCache = cache.find((item) => item.hash === hash);
+
+    if (objectInCache) {
+      console.log("Из кеша: " + objectInCache.value);
+      return "Из кеша: " + objectInCache.value;
+    }
+
+    const result = func(...args);
+
+    cache.push({
+      hash: hash,
+      value: result,
+    });
+
+    if (cache.length > 5) {
+      cache.shift();
+    }
+
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  }
+
+  return wrapper;
+}
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+  let timeoutId = null;
+  let isFirstCall = true;
+
+  function wrapper(...args) {
+    wrapper.allCount++;
+
+    if (isFirstCall) {
+      isFirstCall = false;
+      wrapper.count++;
+      func.apply(this, args);
+      return;
+    }
+
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+
+    timeoutId = setTimeout(() => {
+      wrapper.count++;
+      func.apply(this, args);
+      timeoutId = null;
+    }, delay);
+  }
+
+  wrapper.count = 0;
+  wrapper.allCount = 0;
+
+  return wrapper;
 }
